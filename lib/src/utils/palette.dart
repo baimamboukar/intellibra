d5b6ff6eb3595a2e2b7dbfe01153d3f426efa8be
baimamboukar <baimamboukar@gmail.com>
2022-10-12 23:56:02 +0100

    🧣 Intellibra

---
+++ lib/src/utils/palette.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/rendering.dart';
+
+class Palette {
+  static const Color primary = Color(0xFFFE1696);
+  static const Color secondary = Color(0xFFCC3388);
+  static const Color tertiary = Color(0xFFF9B9D6);
+  static const Color quaternary = Color(0xFFDE67BE);
+  static const Color quinary = Color(0xFFFFCC70);
+  static const Color senary = Color(0xFFF9F871);
+  static const Color backgroundDark = Color(0xFF16213E);
+  // GENERIC PALETTE
+  static const Color dark = Color(0xFF000000);
+  static const Color light = Color(0xFFFFFFFF);
+  static const Color grey = Color(0xFFE0E0E0);
+  static const Color darkGrey = Color(0xFF9E9E9E);
+  static const Color lightGrey = Color(0xFFF5F5F5);
+  static const Color danger = Color(0xFFE53935);
+  static const Color success = Color(0xFF43A047);
+  static const Color info = Color(0xFF1E88E5);
+  static const Color yellow = Color(0xFFFFC107);
+  static const Color orange = Color(0xFFFF9800);
+}
