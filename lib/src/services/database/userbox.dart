a6066914580fba648bd3a101058ffbf01a75da09
baimamboukar <baimamboukar@gmail.com>
2022-10-12 23:56:02 +0100

    🧣 Intellibra

---
+++ lib/src/services/database/userbox.dart
@@ -0,0 +1,20 @@
+import 'package:intellibra/src/models/user_model.dart';
+import 'package:hive_flutter/adapters.dart';
+
+class UserBox {
+  static void add(UserModel user) {
+    if (!(Hive.isBoxOpen('userBox'))) {
+      Hive.openBox<UserModel>('userBox');
+    }
+    Hive.box('userBox').put(user.uid, user.toJson());
+  }
+
+  static UserModel? getUser(String uid) {
+    if (!(Hive.isBoxOpen('userBox'))) {
+      Hive.openBox<UserModel>('userBox');
+    }
+    final user = Hive.box('userBox').get(uid);
+
+    return UserModel.fromJson(user);
+  }
+}
