31dd44ff89779a28500827e4fca045d01d0c09a7
baimamboukar <baimamboukar@gmail.com>
2022-10-12 23:56:02 +0100

    🧣 Intellibra

---
+++ lib/src/screens/screens.dart
@@ -0,0 +1,10 @@
+export 'auth/login.dart';
+
+export 'auth/signup.dart';
+export 'home.dart';
+export 'awareness.dart';
+export 'home.dart';
+export 'welcome.dart';
+export 'self_exam.dart';
+export 'medical_records.dart';
+export 'intellibra.dart';
