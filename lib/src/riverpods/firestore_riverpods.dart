e860915aa0dc02a1daeed9cd3c782a4270f34241
baimamboukar <baimamboukar@gmail.com>
2022-10-12 23:56:01 +0100

    🧣 Intellibra

---
+++ lib/src/riverpods/firestore_riverpods.dart
@@ -0,0 +1,10 @@
+import 'package:intellibra/src/services/firebase/firestore/user_services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class FirestoreRiverpods {
+  static final userServicesRiverpods = Provider<UserServices>(
+    (ref) {
+      return UserServices(ref.read);
+    },
+  );
+}
