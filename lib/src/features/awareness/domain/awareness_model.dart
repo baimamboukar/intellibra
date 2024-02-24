a32131737790b85380563ea91d0f48f144dba95b
chendjou237 <chenxhenor@gmail.com>
2024-02-24 09:40:14 +0100

    Add freezed and json_serializable dependencies

---
+++ lib/src/features/awareness/domain/awareness_model.dart
@@ -0,0 +1,27 @@
+
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'awareness_model.g.dart';
+part 'awareness_model.freezed.dart';
+
+@freezed
+class AwarenessTopic with _$AwarenessTopic{
+  
+const factory  AwarenessTopic({required String id, required String name, required  String description}) = _AwarenessTopic;  
+ factory AwarenessTopic.fromJson(Map<String, dynamic> json) => _$AwarenessTopicFromJson(json);
+}
+
+class Article with _$Article{
+const factory  Article({
+    required String id,
+    required  String title,
+    required String content,
+    required List<AwarenessTopic> topics,
+    required String authorId,
+    required String authorName,
+    required DateTime createdAt,
+    required DateTime updatedAt,
+  })= _Article;
+
+factory Article.fromJson(Map<String, dynamic> json) => _$ArticleFromJson(json);
+}
